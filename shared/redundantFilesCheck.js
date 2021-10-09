85decaf5996755d1e6cb66d330773128f46277f6
Maximilian118 <maxcrosby118@gmail.com>
2021-10-09 17:15:28 +0300

    dbCheckPosts

--- shared/redundantFilesCheck.js
+++ shared/redundantFilesCheck.js
@@ -2,6 +2,7 @@ const aws = require("aws-sdk")
 const moment = require("moment")
 
 const User = require("../models/user")
+const Post = require("../models/post")
 const Geojson = require("../models/geojson")
 
 const { endpoint } = require("./utility")
@@ -20,14 +21,37 @@ const wait = async (ms) => {
   })
 }
 
+const dbCheckPosts = async user => {
+  let newPosts = user.posts
+
+  await Promise.all(user.posts.map(async postID => { // loop through all the posts in user.
+    const post = await Post.findById(postID).populate() // try and find post in the db.
+
+    if (!post) { // If that post doesn't exist in the db.
+      newPosts = newPosts.filter(p => p !== postID) 
+    } else if (moment(post.updated_at).isBefore(moment().subtract(1, 'year'))) { // If post hasn't been updated in a year.
+      newPosts = newPosts.filter(p => p !== postID) 
+      await Post.deleteOne({ _id: postID }) // This post is very old so delete it from the db.
+    }
+  }))
+
+  if (newPosts.length !== user.posts.length) {
+    user.posts = newPosts
+    user.updated_at = moment().format()
+    await user.save()
+  }
+}
+
 const dbCheckGeojsons = async user => {
   let newGeos = user.geojsons
+
   await Promise.all(user.geojsons.map(async geoID => { // loop through all the geojsons in user.
-    const geojson = await Geojson.findById(geoID).populate() // try and find that geojson in the db.
+    const geojson = await Geojson.findById(geoID).populate() // try and find geojson in the db.
 
     if (!geojson) { // If that geojson doesn't exist in the db.
-      newGeos = newGeos.filter(g => g !== geoID) // Remove that geojson from user.geojsons and save the new arr in newGeos.
+      newGeos = newGeos.filter(g => g !== geoID)
     } else if (!geojson.track && !geojson.post) { // Else if that geojson doesn't have a track ID or post ID.
+      newGeos = newGeos.filter(g => g !== geoID)
       await Geojson.deleteOne({ _id: geoID }) // This geojson is useless so delete it from the db.
     }
   }))
@@ -100,6 +124,7 @@ const redundantFilesCheck = async _id => {
 
   // db
   await dbCheckGeojsons(user)
+  await dbCheckPosts(user)
 
   // s3
   await s3CheckPP(user)
